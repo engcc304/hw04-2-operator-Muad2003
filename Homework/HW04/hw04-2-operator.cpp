621b45035eb8011fa2a6b2cac53f81c89254103b
Muad2003 <anuphap2003118@gmail.com>
2023-08-03 17:48:36 +0700

    update homework 4-2

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -13,9 +13,9 @@ int main() {
     printf( "a - b = %d \n", a - b ) ;
     printf( "a * b = %d \n", a * b ) ; 
     printf( "a / b = %d (b must greater than 0)\n", b > 0 ? b : 0 ) ;
-    printf( "a % b = %d \n", a % b ) ;
-    printf( "++a = %d \n", a++ ) ;
-    printf( "a-- = %d \n", a-- ) ;
+    printf( "a %% b = %d \n", a % b ) ;
+    printf( "++a = %d \n", ++a ) ;
+    printf( "a-- = %d \n", --a ) ;
     printf( "a += 1 %d \n",a += 1 ) ;
     printf( "a += b %d \n",a += b ) ;
     printf( "a -= 1 %d \n", a -= 1 ) ;
