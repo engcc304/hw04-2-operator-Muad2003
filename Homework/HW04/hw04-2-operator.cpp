3595e10a3a775ef015fc0f1307f9135f3110570d
Muad2003 <anuphap2003118@gmail.com>
2023-08-04 16:16:39 +0700

    update homework 4-2

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -8,7 +8,7 @@ int main() {
     scanf( "%d %d", &a, &b ) ; //TODO: #11 Input variable 'a' and 'b'
 
     //TODO: #12 Complete operator, and display all operator output. (and fix all bugs.)
-    
+
     printf( "a = %d , b = %d \n", a, b ) ;
     printf( "a + b = %d \n", a + b ) ;
     printf( "a - b = %d \n", a - b ) ;
@@ -23,8 +23,8 @@ int main() {
     printf( "a -= b %d \n", a -= b ) ;
     printf( "a *= 1 %d \n", a *= 1 ) ;
     printf( "a *= b %d \n", a *= b ) ;
-    printf( "a %= 1 %d \n", a %= 1 ) ;
-    printf( "a %= b %d \n", a %= b ) ;
+    printf( "a %%= 1 %d \n", a %= 1 ) ;
+    printf( "a %%= b %d \n", a %= b ) ;
     printf( "a && b %d \n", a && b ) ;
     printf( "a || b %d \n", a || b ) ;
     return 0 ;
